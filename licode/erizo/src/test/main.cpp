bcc3765bd98a35afb83e160ff02b4963651b6b7e
Anil-bohorya <anilkumar.bohorya@gmail.com>
2015-07-20 19:15:15 +0530

    old licode file deleted and new files added

--- licode/erizo/src/test/main.cpp
+++
@@ -1,2 +0,0 @@
-#define BOOST_TEST_MODULE "C++ Unit tests for Erizo"
-#include <boost/test/included/unit_test.hpp>
--- licode/erizo_controller/erizoAgent/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../../erizo/build/erizo
-ulimit -c unlimited
-
-node $*
