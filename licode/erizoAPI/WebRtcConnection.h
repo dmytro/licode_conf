faed946aec2dc8989a84222b924d5fb7514632f9
Anil-bohorya <anilkumar.bohorya@gmail.com>
2015-07-21 09:36:44 +0530

    licode config files added

---
+++ licode/erizoAPI/WebRtcConnection.h
@@ -0,0 +1,97 @@
+#ifndef WEBRTCCONNECTION_H
+#define WEBRTCCONNECTION_H
+
+#include <node.h>
+#include <WebRtcConnection.h>
+#include "MediaDefinitions.h"
+#include "OneToManyProcessor.h"
+
+
+/*
+ * Wrapper class of erizo::WebRtcConnection
+ *
+ * A WebRTC Connection. This class represents a WebRtcConnection that can be established with other peers via a SDP negotiation
+ * it comprises all the necessary ICE and SRTP components.
+ */
+class WebRtcConnection : public node::ObjectWrap, erizo::WebRtcConnectionEventListener, erizo::WebRtcConnectionStatsListener  {
+ public:
+  static void Init(v8::Handle<v8::Object> target);
+
+  erizo::WebRtcConnection *me;
+  int eventSt;
+  std::queue<int> eventSts;
+  std::queue<std::string> eventMsgs, statsMsgs;
+
+  boost::mutex mutex;
+
+ private:
+  WebRtcConnection();
+  ~WebRtcConnection();
+  
+  v8::Persistent<v8::Function> eventCallback_;
+  v8::Persistent<v8::Function> statsCallback_;
+
+  uv_async_t async_;
+  uv_async_t asyncStats_;
+  bool hasCallback_;
+  /*
+   * Constructor.
+   * Constructs an empty WebRtcConnection without any configuration.
+   */
+  static v8::Handle<v8::Value> New(const v8::Arguments& args);
+  /*
+   * Closes the webRTC connection.
+   * The object cannot be used after this call.
+   */
+  static v8::Handle<v8::Value> close(const v8::Arguments& args);
+  /*
+   * Inits the WebRtcConnection and passes the callback to get Events.
+   * Returns true if the candidates are gathered.
+   */
+  static v8::Handle<v8::Value> init(const v8::Arguments& args);  
+  /*
+   * Sets the SDP of the remote peer.
+   * Param: the SDP.
+   * Returns true if the SDP was received correctly.
+   */
+  static v8::Handle<v8::Value> setRemoteSdp(const v8::Arguments& args);
+  /**
+     * Add new remote candidate (from remote peer).
+     * @param sdp The candidate in SDP format.
+     * @return true if the SDP was received correctly.
+     */
+  static v8::Handle<v8::Value>  addRemoteCandidate(const v8::Arguments& args);
+  /*
+   * Obtains the local SDP.
+   * Returns the SDP as a string.
+   */
+  static v8::Handle<v8::Value> getLocalSdp(const v8::Arguments& args);
+  /*
+   * Sets a MediaReceiver that is going to receive Audio Data
+   * Param: the MediaReceiver to send audio to.
+   */
+  static v8::Handle<v8::Value> setAudioReceiver(const v8::Arguments& args);
+  /*
+   * Sets a MediaReceiver that is going to receive Video Data
+   * Param: the MediaReceiver
+   */
+  static v8::Handle<v8::Value> setVideoReceiver(const v8::Arguments& args);
+  /*
+   * Gets the current state of the Ice Connection
+   * Returns the state.
+   */
+  static v8::Handle<v8::Value> getCurrentState(const v8::Arguments& args);
+
+  static v8::Handle<v8::Value> generatePLIPacket(const v8::Arguments& args);
+
+
+  static v8::Handle<v8::Value> getStats(const v8::Arguments& args);  
+
+  static void eventsCallback(uv_async_t *handle, int status);
+  static void statsCallback(uv_async_t *handle, int status);
+ 
+	virtual void notifyEvent(erizo::WebRTCEvent event, const std::string& message="");
+	virtual void notifyStats(const std::string& message);
+};
+
+#endif
